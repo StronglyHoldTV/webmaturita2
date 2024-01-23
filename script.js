a3d574f7e070fcd8980b7d0b42d723d6230b4acc
StronglyHoldTV <marek.villim@gmail.com>
2024-01-23 07:03:47 +0100

    update

---
+++ script.js
@@ -0,0 +1,27 @@
+var app = document.getElementById('type-writer');
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+});
+
+
+console.log(app)
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="font-family:Times New Roman; font-size: 20pt;"><b>Vitaj slováčisko!</b></span>')
+  .pauseFor(300)
+  .typeString('<br>Som Ľudovít Štúr.')
+  .pauseFor(300)
+  .typeString('<br>Chceš sa niečo dozvedieť o našej <b>literatúre</b>?')
+  .pauseFor(1000)
+  .deleteChars('našej literatúre?'.length)
+  .typeString('mojom <b>živote</b>?')
+  .start();
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
